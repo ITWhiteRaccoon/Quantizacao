e047fb8e27e406252e72b341b09a0e70804fb9c0
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-04-09 22:22:49 -0300

    Changed instructions

--- Program.cs
+++ Program.cs
@@ -9,14 +9,20 @@ namespace Quantizacao
         {
             if (args.Length < 3)
             {
-                Console.WriteLine("usage: quantize <inputImage> <outputImage> <numerOfColors>");
+                Console.WriteLine("usage: Quantizacao <inputImage> <outputImage> <numerOfColors>");
+                return;
             }
 
             string inputImage = args[0].Trim(), outputImage = args[1].Trim();
             int numberOfColors = Convert.ToInt32(args[2].Trim());
 
+            Console.WriteLine($"=== {"",5}Lendo Arquivo{"",5} ===");
             Bitmap bitmap = PPM.ReadToBitmap(inputImage);
+
+            Console.WriteLine($"=== {"",6}Quantizando{"",6} ===");
             bitmap = PPM.Quantize(bitmap, numberOfColors);
+
+            Console.WriteLine($"=== {"",3}Gravando Arquivo{"",4} ===");
             PPM.WriteFromBitmap(outputImage, bitmap);
         }
     }
